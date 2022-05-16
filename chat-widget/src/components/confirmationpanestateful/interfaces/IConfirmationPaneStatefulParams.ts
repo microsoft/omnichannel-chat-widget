86427981e8f2aab9dfd87481b2079cc6972e75e3
Abhinab Mohanty <amohanty@microsoft.com>
2022-05-16 11:53:59 -0700

    Addressing comments

--- chat-widget/src/components/confirmationpanestateful/interfaces/IConfirmationPaneStatefulParams.ts
+++ chat-widget/src/components/confirmationpanestateful/interfaces/IConfirmationPaneStatefulParams.ts
@@ -8,6 +8,9 @@ export interface IConfirmationPaneStatefulParams extends IConfirmationPaneStatef
 
     /**
      * endChat: Internal Prop injected for triggering end of a chat using chatSDK
+     * @param adapter : The chat adapter for the live chat session
+     * @param skipEndChatSDK : If set to true endchat will skip chatSDK endChat call
+     * @param skipCloseChat : If set to true endchat will skip closing the live chat instance
      */
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     endChat: (adapter: any, skipEndChatSDK?: boolean, skipCloseChat?: boolean) => Promise<void>;
