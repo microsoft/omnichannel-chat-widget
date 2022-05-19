ec7adbf50761526acecca844e800ee8e1fe2cbae
Mahadev Arwat <v-maarw@microsoft.com>
2022-05-18 18:31:52 -0700

    framework

---
+++ chat-widget/automation_tests/configuration/CustomEnvironment.js
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const PlaywrightEnvironment =
+  require("jest-playwright-preset/lib/PlaywrightEnvironment").default;
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require("fs");
+class CustomEnvironment extends PlaywrightEnvironment {
+    async handleTestEvent(event) {
+        if (event.name === "test_done" && event.test.errors.length > 0) {
+            try {
+                const parentName = event.test.parent.name.replace(/\W/g, "_");
+                const specName = event.test.name.replace(/\W/g, "_");
+                const contexts = this.global.browser.contexts();
+                for (let [contextIndex, currentContext] of contexts.entries()) {
+                    const contextPages = currentContext.pages();
+                    for (let [pageIndex, currentPage] of contextPages.entries()) {
+                        if (currentPage) {
+                            try {
+                                await currentPage.screenshot({
+                                    path: `screenshots/${parentName}-${specName}_context${contextIndex}_page${pageIndex}_${new Date().getUTCHours()}-${new Date().getUTCMinutes()}.png`,
+                                    fullPage: true,
+                                    type: "png",
+                                });
+                                await currentPage.close();
+                            } catch (e) {
+                                console.log(
+                                    `screenshot failed test: ${specName}_page \n ${e}`
+                                );
+                            }
+                        }
+                    }
+                }
+            // eslint-disable-next-line no-empty
+            } catch (e) {}
+        }
+    }
+}
+
+module.exports = CustomEnvironment;
