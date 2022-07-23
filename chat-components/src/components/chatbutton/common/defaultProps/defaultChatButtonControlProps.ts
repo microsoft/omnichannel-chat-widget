e19f561a1d5d80c7b395eeeedb8b2d63285a21ea
dishakhattri <dishakhattri@gmail.com>
2022-07-22 17:32:35 -0700

    Update defaultChatButtonControlProps.ts

--- chat-components/src/components/chatbutton/common/defaultProps/defaultChatButtonControlProps.ts
+++ chat-components/src/components/chatbutton/common/defaultProps/defaultChatButtonControlProps.ts
@@ -16,5 +16,6 @@ export const defaultChatButtonControlProps: IChatButtonControlProps = {
     hideChatTextContainer: false,
     hideChatSubtitle: false,
     hideChatTitle: false,
-    hideNotificationBubble: true
+    hideNotificationBubble: true,
+    unreadMessageString: "new messages"
 };
