7719d0ed4d63847f495966016f86d522222319c0
dishakhattri <dishakhattri@gmail.com>
2022-07-25 18:54:49 -0700

    Update IChatButtonControlProps.ts

--- chat-components/src/components/chatbutton/interfaces/IChatButtonControlProps.ts
+++ chat-components/src/components/chatbutton/interfaces/IChatButtonControlProps.ts
@@ -14,4 +14,5 @@ export interface IChatButtonControlProps {
     hideChatTitle?: boolean;
     hideNotificationBubble?: boolean;
     unreadMessageString?: string;
+    largeUnreadMessageString?: string;
 }
