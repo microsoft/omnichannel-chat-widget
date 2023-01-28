67f6c719e718e58309d8be5b2b5a0693bc68717d
Saroj <sarojpradhan@microsoft.com>
2023-01-27 20:02:37 -0500

    Updating telemetry

--- chat-widget/src/plugins/newMessageEventHandler.ts
+++ chat-widget/src/plugins/newMessageEventHandler.ts
@@ -46,6 +46,10 @@ export const createOnNewAdapterActivityHandler = (chatId: string, userId: string
                 if (activity?.channelData?.tags?.includes(Constants.systemMessageTag)) {
                     // eslint-disable-next-line @typescript-eslint/no-explicit-any
                     payload.messageType = Constants.systemMessageTag;
+                    TelemetryHelper.logActionEvent(LogLevel.INFO, {
+                        Event: TelemetryEvent.SystemMessageReceived,
+                        Description: "System message received"
+                    });
                 }
                 else {
                     // eslint-disable-next-line @typescript-eslint/no-explicit-any
