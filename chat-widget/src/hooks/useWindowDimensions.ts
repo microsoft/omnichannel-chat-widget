74d913104f380cac5f1109e77ebedac24b698501
Josue Hernandez Mateo <josuhern@microsoft.com>
2023-02-06 08:31:35 -0800

    making better

--- chat-widget/src/hooks/useWindowDimensions.ts
+++ chat-widget/src/hooks/useWindowDimensions.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function getWindowDimensions() {
     const { innerWidth: width, innerHeight: height } = window;
