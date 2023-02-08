bc556cf439a783c34157af0fe6444d8e9a5c86fa
Josue Hernandez Mateo <josuhern@microsoft.com>
2023-02-07 20:17:25 -0800

    adding debounce

--- chat-widget/src/hooks/useWindowDimensions.ts
+++ chat-widget/src/hooks/useWindowDimensions.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import useDebounce from "./useDebounce2";
 
 function getWindowDimensions() {
     const { innerWidth: width, innerHeight: height } = window;
@@ -8,16 +9,16 @@ function getWindowDimensions() {
     };
 }
 
-export default function useWindowDimensions() {
+export default function useWindowDimensions(delay = 200) {
+  
     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
     useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
-
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
+        const handleResize = () => setWindowDimensions(getWindowDimensions());
+        const debouncedHandleResize = useDebounce(handleResize, delay);
+        
+        window.addEventListener("resize", debouncedHandleResize);
+        return () => window.removeEventListener("resize", debouncedHandleResize);
     }, []);
 
     return windowDimensions;
