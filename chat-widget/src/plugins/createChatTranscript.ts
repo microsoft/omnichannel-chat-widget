208d2ea80d671b034eda547af5dcde15ac0ea7ca
Edward Tran <edtran@microsoft.com>
2023-04-21 14:21:39 -0700

    Add TranscriptHTMLBuilder

--- chat-widget/src/plugins/createChatTranscript.ts
+++ chat-widget/src/plugins/createChatTranscript.ts
@@ -1,7 +1,143 @@
+class TranscriptHTMLBuilder {
+    createHeadElement() {
+        const htmlData = `
+            <head>
+                <title> Chat Transcript </title>
+                <script src="https://cdn.botframework.com/botframework-webchat/4.15.7/webchat.js"><\/script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/rxjs/7.8.0/rxjs.umd.min.js" integrity="sha512-v0/YVjBcbjLN6scjmmJN+h86koeB7JhY4/2YeyA5l+rTdtKLv0VbDBNJ32rxJpsaW1QGMd1Z16lsLOSGI38Rbg==" crossorigin="anonymous" referrerpolicy="no-referrer"><\/script>
+                <script>
+                    function shareObservable(observable) {
+                        let observers = [];
+                        let subscription;
+
+                        return new Observable(observer => {
+                            if (!subscription) {
+                                subscription = observable.subscribe({
+                                    complete() {
+                                        observers.forEach(observer => observer.complete());
+                                    },
+                                    error(err) {
+                                        observers.forEach(observer => observer.error(err));
+                                    },
+                                    next(value) {
+                                        observers.forEach(observer => observer.next(value));
+                                    }
+                                });
+                            }
+
+                            observers.push(observer);
+
+                            return () => {
+                                observers = observers.filter(o => o !== observer);
+
+                                if (!observers.length) {
+                                    subscription.unsubscribe();
+                                    subscription = null;
+                                }
+                            };
+                        });
+                    }
+                <\/script>
+                <script>
+                    const sampleMessages = [
+                        'one',
+                        'two',
+                        'three',
+                        'four',
+                        'five'
+                    ];
+                <\/script>
+                <script>
+                    class TranscriptAdapter {
+                        constructor() {
+                            this.connectionStatus$ = new window.rxjs.BehaviorSubject(0); // Unitialized
+                            this.activity$ = shareObservable(new Observable((observer) => {
+                                this.activityObserver = observer;
+
+                                this.connectionStatus$.next(1); // Connecting
+                                this.connectionStatus$.next(2); // Online
+
+                                // Retrieve messages
+                                if (sampleMessages) {
+                                    setTimeout(() => { // setTimeout is needed due to some WebChat issues
+                                        sampleMessages.map((message) => {
+                                            this.activityObserver.next({
+                                                from: {
+                                                    role: 'user'
+                                                },
+                                                text: message,
+                                                type: 'message',
+                                            });
+                                        });
+                                    }, 1);
+                                }
+                            }));
+                        }
+                    }
+                <\/script>
+            </head>
+        `;
+
+        return htmlData;
+    }
+
+    createBodyElement() {
+        const htmlData = `
+            <body>
+                <div id="transcript"></div>
+                <script>
+                    const adapter = new TranscriptAdapter();
+                    const styleOptions = {
+                        hideSendBox: true
+                    };
+
+                    window.WebChat.renderWebChat({
+                        directLine: adapter,
+                        styleOptions
+                    },
+                    document.getElementById('transcript'));
+                <\/script>
+            </body>
+        `;
+
+        return htmlData;
+    }
+
+    createHTML() {
+        const htmlData = `
+            <html>
+                ${this.createHeadElement()}
+                ${this.createBodyElement()}
+            </html>
+        `;
+
+        return htmlData;
+    }
+}
+
+const download = (fileName: string, htmlData: string) => {
+    const aElement = document.createElement("a");
+
+    const blob = new Blob([htmlData], {type: "text/html"});
+    const objectUrl = URL.createObjectURL(blob);
+
+    aElement.setAttribute("href", objectUrl);
+    aElement.setAttribute("download", fileName);
+    aElement.style.display = "none";
+
+    document.body.appendChild(aElement);
+    aElement.click();
+    document.body.removeChild(aElement);
+};
+
 const createChatTranscript = (transcript: string) => {
     console.log("[createChatTranscript]");
     const data = JSON.parse(transcript);
     console.log(data);
+
+    const htmlBuilder = new TranscriptHTMLBuilder();
+    const text = htmlBuilder.createHTML();
+    download("transcript.html", text);
 };
 
 export default createChatTranscript;
