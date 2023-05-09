8c1ecc176b950512257441972bd2c78376cc1a44
Edward Tran <edtran@microsoft.com>
2023-05-08 20:16:37 -0700

    Add doc

--- chat-widget/src/components/livechatwidget/interfaces/IDraggableChatWidgetProps.ts
+++ chat-widget/src/components/livechatwidget/interfaces/IDraggableChatWidgetProps.ts
@@ -1,4 +1,10 @@
 export interface IDraggableChatWidgetProps {
+    /**
+     * Whether to enable Draggable Chat Widget.
+     */
     disable?: boolean;
+    /**
+     * To specify whether the iframe is used to drag. The DraggableEvent would be posted to the parent window if set to true.
+     */
     targetIframe?: boolean;
 }
