48684449052f96c04e0b4eace25c7677984144be
Edward Tran <edtran@microsoft.com>
2023-06-12 16:18:53 -0700

    Refactor

--- chat-widget/src/components/draggable/IDraggableEvent.ts
+++ chat-widget/src/components/draggable/IDraggableEvent.ts
@@ -1,10 +1,11 @@
 import DraggableEventNames from "./DraggableEventNames";
+import IDraggableElementPosition from "./IDraggableElementPosition";
 
 interface IDraggableEvent {
     channel: string;
     eventName: DraggableEventNames | string;
     offset?: { x: number; y: number };
-    position?: {offsetLeft: number, offsetTop: number};
+    position?: IDraggableElementPosition;
 }
 
 export default IDraggableEvent;
