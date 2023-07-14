c527d7e708499228c93388448820962e52ea728b
Edgar Lopez <elopezanaya@gmail.com>
2023-07-14 16:54:07 -0700

    format

--- chat-components/src/components/inputvalidationpane/InputValidationPane.tsx
+++ chat-components/src/components/inputvalidationpane/InputValidationPane.tsx
@@ -237,7 +237,7 @@ function InputValidationPane(props: IInputValidationPaneProps) {
                             borderless={isInvalidInput}
                             onChange={handleInputChange}
                             onKeyUp={handleInputKeyUp}
-                            placeholder={props.controlProps?.InputPlaceholder || defaultInputValidationPaneControlProps.InputPlaceholder}
+                            placeholder={props.controlProps?.inputPlaceHolder || defaultInputValidationPaneControlProps.inputPlaceHolder}
                         />) }
 
                         { isInvalidInput && (decodeComponentString(props.componentOverrides?.invalidInputErrorMessage) ||
