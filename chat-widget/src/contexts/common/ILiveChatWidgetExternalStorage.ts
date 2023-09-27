062574c6b46564b3eafdf28b1b12f13b58d47d8c
Edgar Lopez <elopezanaya@gmail.com>
2023-09-27 12:49:46 -0700

    adding new Interface for external storage

--- chat-widget/src/contexts/common/ILiveChatWidgetExternalStorage.ts
+++ chat-widget/src/contexts/common/ILiveChatWidgetExternalStorage.ts
@@ -1,6 +1,7 @@
 
 export interface ILiveChatWidgetExternalStorage {
-
     useExternalStorage: boolean | false;
     timeOutWaitForResponse: number | 1000;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    cachedData : any | null;
 }
