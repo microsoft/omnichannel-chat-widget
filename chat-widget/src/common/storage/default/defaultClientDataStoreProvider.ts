20d342590600abdaed53be414cf5a926674279d1
Edgar Lopez <elopezanaya@gmail.com>
2023-09-27 12:33:38 -0700

    cleanup, defaultClientDataStore will emit events only for SetData

--- chat-widget/src/common/storage/default/defaultClientDataStoreProvider.ts
+++ chat-widget/src/common/storage/default/defaultClientDataStoreProvider.ts
@@ -8,7 +8,7 @@ import { TelemetryHelper } from "../../telemetry/TelemetryHelper";
 import { inMemoryDataStore } from "./defaultInMemoryDataStore";
 import { BroadcastService } from "@microsoft/omnichannel-chat-components";
 
-export const isCookieAllowed = (isUsingExternalStorage : boolean) => {
+export const isCookieAllowed = (isUsingExternalStorage: boolean) => {
     try {
         localStorage;
         sessionStorage;
@@ -32,6 +32,7 @@ export const isCookieAllowed = (isUsingExternalStorage : boolean) => {
 export const defaultClientDataStoreProvider = (cacheTtlinMins = 0, storageType: StorageType = StorageType.localStorage, useExternalStorage?: boolean, timeOut?: number | 1000): IContextDataStore => {
     let ttlInMs = 0;
     const switchToExternalStorage = useExternalStorage || false;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const timeOutWaitForResponse = timeOut || 1000;
 
     if (ttlInMs == 0) {
@@ -66,15 +67,12 @@ export const defaultClientDataStoreProvider = (cacheTtlinMins = 0, storageType: 
                 }
             } else {
                 const dataToCache = {
-                    messageName: "external:saveData",
                     key: key,
                     data: data,
-                    type: (storageType == StorageType.localStorage ? "localStorage" : "sessionStorage")
                 };
-
                 //Emit the data to be cached to the external storage
                 BroadcastService.postMessage({
-                    eventName: BroadcastEvent.NotifyExternalSaveData,
+                    eventName: BroadcastEvent.ExternalSaveDataRequest,
                     payload: dataToCache
                 });
             }
@@ -103,49 +101,22 @@ export const defaultClientDataStoreProvider = (cacheTtlinMins = 0, storageType: 
                     return itemInJson.data;
                 }
             } else {
-                // get data from in memory db when cookie is disabled
-                let result = inMemoryDataStore().getData(key);
-                let ack = false;
-
-                // we are not using promise since is not an async function, we are using a while loop to wait for the response
-                const waitForResponseOrTimeOut = (miliseconds: number) => {
-                    const start = new Date().getTime();
-                    const end = start + miliseconds;
-                    // eslint-disable-next-line no-empty
-                    while ((new Date().getTime() < end) && !ack) {}
-                };
-
-                //only switch to events when customer initiates the switch
-                if (result === null && switchToExternalStorage) {
-                    //we are listening to the event to receive the data from external storage
-                    BroadcastService.getMessageByEventName(BroadcastEvent.ReceiveExternalItemData).subscribe((data) => {
-                        if (data.payload.key === key) {
-                            result = data.payload.data;
-                            // we save the data in the in memory db
-                            inMemoryDataStore().setData(key, result);
-                            //indicates the response was received, we can stop waiting
-                            ack = true;
-                        }
-                    });
-                    // we are sending the request to external storage to get the data
-                    BroadcastService.postMessage({
-                        eventName: BroadcastEvent.RequestExternalItemData,
-                        payload: {
-                            key: key
-                        }
-                    });
-                    // we are waiting for the response or timeout
-                    waitForResponseOrTimeOut(timeOutWaitForResponse);
-
-                    if (!ack){
-                        TelemetryHelper.logConfigDataEvent(LogLevel.ERROR, {
-                            Event: TelemetryEvent.ClientDataStoreProviderFailed,
-                            ExceptionDetails: "External storage did not respond in time for key " + key + ".    Timeout: " + timeOutWaitForResponse + " ms",
-                            Description: "External storage did not respond in time for key " + key + ".    Timeout: " + timeOutWaitForResponse + " ms"
-                        });
+                
+                const result = inMemoryDataStore().getData(key);
+                let itemInJson = undefined;
+                if (result !== null && result !== undefined) {
+                    itemInJson = JSON.parse(result);
+                    const now = new Date();
+                    // compare the expiry time of the item with the current time
+                    if (now.getTime() > itemInJson.expiry) {
+                        // If the item is expired, delete the item from storage
+                        // and return null
+                        localStorage.removeItem(key);
+                        return null;
                     }
+                    return itemInJson.data;
                 }
-                return result;
+                return itemInJson;
             }
         },
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
