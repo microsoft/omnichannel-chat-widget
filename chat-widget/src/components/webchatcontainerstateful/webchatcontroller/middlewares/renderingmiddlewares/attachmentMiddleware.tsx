04b6681fe933732479b16e9398d037d23ff0707e
Edward Tran <edtran@microsoft.com>
2023-09-28 10:24:08 -0700

    Add styling for attachments in progress of file scanning

--- chat-widget/src/components/webchatcontainerstateful/webchatcontroller/middlewares/renderingmiddlewares/attachmentMiddleware.tsx
+++ chat-widget/src/components/webchatcontainerstateful/webchatcontroller/middlewares/renderingmiddlewares/attachmentMiddleware.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /******
  * AttachmentMiddleware
- * 
+ *
  * Handles attachment downloading.
  ******/
 
 import { Constants, MimeTypes, WebChatMiddlewareConstants } from "../../../../../common/Constants";
 import React, { Dispatch } from "react";
 import { getFileAttachmentIconData, isInlineMediaSupported } from "../../../common/utils/FileAttachmentIconManager";
-
 import { BroadcastEvent } from "../../../../../common/telemetry/TelemetryConstants";
 import { BroadcastService } from "@microsoft/omnichannel-chat-components";
 import { ILiveChatWidgetAction } from "../../../../../contexts/common/ILiveChatWidgetAction";
@@ -24,6 +23,7 @@ import { defaultAttachmentProps } from "../../../common/defaultProps/defaultAtta
 import { defaultAttachmentSizeStyles } from "./defaultStyles/defaultAttachmentSizeStyles";
 import { defaultAttachmentStyles } from "./defaultStyles/defaultAtttachmentStyles";
 import { useChatContextStore } from "../../../../..";
+import { FileScanInProgressIcon, MaliciousFileIcon } from "../../../../../assets/Icons";
 
 const AttachmentContent = (props: any) => {
     return (
@@ -48,7 +48,7 @@ const Attachment = (props: any) => {
     const attachmentDividerStyles = {...defaultAttachmentDividerStyles, ...state.domainStates.renderingMiddlewareProps?.attachmentDividerStyles};
     const attachmentIconStyles = {...defaultAttachmentIconStyles, ...state.domainStates.renderingMiddlewareProps?.attachmentIconStyles};
     const attachmentStyles = {...defaultAttachmentStyles, ...state.domainStates.renderingMiddlewareProps?.attachmentStyles};
-    
+
     const attachmentSizeStylesString = (
         Object.entries({...defaultAttachmentSizeStyles, ...state.domainStates.renderingMiddlewareProps?.attachmentSizeStyles}).map(([k, v]) => `${k.replace(/[A-Z]/g, (match: string) => `-${match.toLowerCase()}`)}:${v}`).join(";"));
     const attachmentContentStylesString = (
@@ -57,7 +57,7 @@ const Attachment = (props: any) => {
         Object.entries({...defaultAttachmentFileNameStyles, ...state.domainStates.renderingMiddlewareProps?.attachmentFileNameStyles}).map(([k, v]) => `${k.replace(/[A-Z]/g, (match: string) => `-${match.toLowerCase()}`)}:${v}`).join(";"));
     const attachmentDownloadIconStylesString = (
         Object.entries({...defaultAttachmentDownloadIconStyles, ...state.domainStates.renderingMiddlewareProps?.attachmentDownloadIconStyles}).map(([k, v]) => `${k.replace(/[A-Z]/g, (match: string) => `-${match.toLowerCase()}`)}:${v}`).join(";"));
-    
+
     return (
         <><style>{`
             .webchat__fileContent__size { ${attachmentSizeStylesString} }
@@ -77,6 +77,62 @@ const Attachment = (props: any) => {
     );
 };
 
+const Spinner = (props: any) => {
+    const spinnerStyle: any = {
+        boxSizing: "border-box",
+        borderRadius: "50%",
+        borderWidth: props.borderWidth || 2,
+        borderStyle: "solid",
+        borderColor: "rgb(0,120,212) rgb(199,224,244) rgb(199,224,244)",
+        borderImage: "initial",
+        animationName: "spin",
+        animationDuration: "1.3s",
+        animationIterationCount: "infinite",
+        animationTimingFunction: "cubic-bezier(0.53, 0.21, 0.29, 0.67)",
+        width: props.size || 20,
+        height: props.size || 20,
+    };
+
+    return (
+        <>
+            <style>{`
+                @keyframes spin {
+                    0% {
+                        transform: rotate(0deg);
+                    }
+                    100% {
+                        transform: rotate(360deg);
+                    }
+                }
+            `}</style>
+            <div className="spinner" style={spinnerStyle}></div>
+        </>
+    );
+};
+
+const ScanInProgressAttachment = (props: any) => {
+    console.log("[ScanInProgressAttachment]");
+    const renderer = () => (
+        <div style={{display: "flex", padding: 10, width: "100%"}}>
+            <div style={{fontSize: 12, fontFamily: "Segoe UI, Arial, sans-serif"}}> {props.textCard.attachment.name} </div>
+            <div style={{marginLeft: "auto"}}>
+                <Spinner size={16}/>
+            </div>
+        </div>
+    );
+
+    return (
+        <Attachment {...props} imageCard={undefined} renderer={renderer}/>
+    );
+};
+
+const MaliciousAttachment = (props: any) => {
+    console.log("[MaliciousAttachment]");
+    return (
+        <Attachment {...props} imageCard={undefined}/>
+    );
+};
+
 /**
 * Patch card with different attachment data.
 * @param card
@@ -87,7 +143,7 @@ const patchAttachment = (card : any, newAttachment: any) => {
     const patchedAttachment = Object.assign({}, attachment);
     patchedAttachment.contentType = newAttachment.contentType;
     patchedAttachment.thumbnailUrl = newAttachment.thumbnailUrl;
-    
+
     const patchedAttachments = activity.attachments.map((target: any) =>
         target === attachment ? patchedAttachment : target
     );
@@ -110,25 +166,25 @@ const genPreviewCardWithAttachment = (card: any, iconData: any, next: any) => {
     );
 };
 
-const createAttachmentMiddleware = (enableInlinePlaying: boolean | undefined) => { 
-    // eslint-disable-next-line react/display-name   
+const createAttachmentMiddleware = (enableInlinePlaying: boolean | undefined) => {
+    // eslint-disable-next-line react/display-name
     const attachmentMiddleware = () => (next: any) => (...args: any) => {
         const [card] = args;
         if (!card?.activity) {
             return next(...args);
         }
-    
+
         const { activity: { attachments }, attachment } : {activity: { attachments: any}, attachment: any} = card;
         // No attachment
         if (!attachments || !attachments.length || !attachment) {
             return next(...args);
         }
-    
+
         // Check for Adaptive cards
         // eslint-disable-next-line prefer-const
         let { content, contentType } = attachment || { content: "", contentType: "" };
         let { type } = content || { type: "" };
-    
+
         if (!type && content && Constants.supportedAdaptiveCardContentTypes.indexOf(contentType) >= 0) {
             try {
                 content = JSON.parse(content);
@@ -159,7 +215,7 @@ const createAttachmentMiddleware = (enableInlinePlaying: boolean | undefined) =>
         } else if (contentType.startsWith(Constants.adaptiveCardContentTypePrefix)) {
             console.warn(`${contentType} adaptive card type is currently not supported.`);
         }
-    
+
         if (card.activity.channelData?.middlewareData) {
             attachment.contentUrl = card.activity.channelData.middlewareData[attachment.name];
         } else if (attachment?.tempContentUrl) {
@@ -170,22 +226,45 @@ const createAttachmentMiddleware = (enableInlinePlaying: boolean | undefined) =>
             return next(...args);
         }
 
+        if (card.activity.channelData && card.activity.channelData.fileScan) {
+            console.log("[AttachmentMiddleware][FileScan]");
+            console.log(card.activity);
+            const index = attachments.findIndex((attachment: any) => (attachment.name === card.attachment.name));
+            const {activity: {channelData: {fileScan}}} = card;
+
+            let scanResult = undefined;
+            scanResult = fileScan[index];
+
+            if (scanResult?.status === "in progress") {
+                return (
+                    <ScanInProgressAttachment iconData={FileScanInProgressIcon} textCard={card} />
+                );
+            }
+
+            if (scanResult?.status === "malware") {
+                return (
+                    <MaliciousAttachment iconData={MaliciousFileIcon} textCard={card} />
+                );
+            }
+        }
+
         const fileExtension = attachment.name.substring(attachment.name.lastIndexOf(".") + 1, attachment.name.length) || attachment.name;
         const imageExtension = Constants.imageRegex.test(attachment.name);
-    
+
         const audioExtension = Constants.audioMediaRegex.test(attachment.name);
         const videoExtension = Constants.videoMediaRegex.test(attachment.name);
-               
+
         const iconData = getFileAttachmentIconData(fileExtension);
+
         if (imageExtension) {
             return genPreviewCardWithAttachment(card, iconData, next);
         }
-    
+
         if (audioExtension || videoExtension){
             if (enableInlinePlaying && card.activity.actionType && card.activity.actionType === WebChatActionType.DIRECT_LINE_INCOMING_ACTIVITY &&  isInlineMediaSupported(attachment.name)) {
                 return genPreviewCardWithAttachment(card, iconData, next);
             }
-            
+
             return (
                 <Attachment
                     iconData={iconData}
@@ -195,7 +274,7 @@ const createAttachmentMiddleware = (enableInlinePlaying: boolean | undefined) =>
             );
         }
 
-        const isUnknownImageObject = contentType.toLowerCase().includes("image") && !imageExtension;    
+        const isUnknownImageObject = contentType.toLowerCase().includes("image") && !imageExtension;
         if (fileExtension === "txt" || isUnknownImageObject) {
             return (
                 <Attachment
@@ -205,7 +284,7 @@ const createAttachmentMiddleware = (enableInlinePlaying: boolean | undefined) =>
                 />
             );
         }
-    
+
         return (
             <Attachment iconData={iconData} textCard={card} renderer={next} />
         );
