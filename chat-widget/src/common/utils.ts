fdb20768ed1dba9beef7b9d71d093eb26e7c3937
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-05-15 15:38:53 -0700

    Add nullable

--- chat-widget/src/common/utils.ts
+++ chat-widget/src/common/utils.ts
@@ -450,7 +450,7 @@ export const parseLowerCaseString = (property: string | boolean | undefined): st
 };
 
 export const setOcUserAgent = (chatSDK: any): void => { // eslint-disable-line @typescript-eslint/no-explicit-any
-    if (chatSDK.OCClient.ocUserAgent && !chatSDK.OCClient.ocUserAgent.join(" ").includes("omnichannel-chat-widget/")) {
+    if (chatSDK.OCClient?.ocUserAgent && !chatSDK.OCClient?.ocUserAgent.join(" ").includes("omnichannel-chat-widget/")) {
         try {
             const version = require("../../../package.json").version; // eslint-disable-line @typescript-eslint/no-var-requires
             const userAgent = `omnichannel-chat-widget/${version}`;
