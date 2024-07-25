fde70337aa96d860be870467e9a6f07aeba73cb7
rakshitha-ballal <89422364+rakshitha-ballal@users.noreply.github.com>
2024-07-25 06:40:57 -0700

    BUG-3831936- Fixed false positive telemetry logs when PostChatSurvey is disabled (#496)
    * BUG 3831936- Fixed false positive telemetry logs when  postChatSurvey Disabled
    
    * Minor refactors
    
    * BUG-3831936: Updated code based on the review
    
    * BUG-3831936: Updated the code based on code review comments
    
    * BUG-3831936: updated postChatEnabled verbiage based the code review comment
    
    * BUG-3831936- Fixed code for startChat.ts
    
    * BUG-3831936:Fixed code related to build issues
    
    * BUG-3831936 - fixed merge conflicts
    
    * BUG-3831936-updated rhe Try Catch lines based on code review
    
    ---------
    
    Co-authored-by: rakshita.ballal <rakshita.ballal@nuance.com>
    Co-authored-by: Matan Hershberg <mhershberg@microsoft.com>

---
+++ chat-widget/src/components/livechatwidget/common/liveChatConfigUtils.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const isPostChatSurveyEnabled = async (chatSDK: any) : Promise<boolean> => {
+    const chatConfig = await chatSDK.getLiveChatConfig();
+    const postChatEnabled = chatConfig.LiveWSAndLiveChatEngJoin
+        ?.msdyn_postconversationsurveyenable.toString().toLowerCase();
+    return postChatEnabled === "true";
+};
