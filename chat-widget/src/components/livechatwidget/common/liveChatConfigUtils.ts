625459943bf5614141f5a1ff4368436d51576522
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-09-04 15:10:30 -0700

    Add util to verify whether persistent chat is enabled

--- chat-widget/src/components/livechatwidget/common/liveChatConfigUtils.ts
+++ chat-widget/src/components/livechatwidget/common/liveChatConfigUtils.ts
@@ -5,3 +5,10 @@ export const isPostChatSurveyEnabled = async (chatSDK: any) : Promise<boolean> =
         ?.msdyn_postconversationsurveyenable.toString().toLowerCase();
     return postChatEnabled === "true";
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const isPersistentChatEnabled = async (chatSDK: any): Promise<boolean> => {
+    const chatConfig = await chatSDK.getLiveChatConfig();
+    const conversationMode = chatConfig.LiveWSAndLiveChatEngJoin?.msdyn_conversationmode.toString().toLowerCase();
+    return conversationMode === "192350001";
+};
