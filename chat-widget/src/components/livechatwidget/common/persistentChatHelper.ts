a5075d3f151f2190e0e74fc7ae29bfe008a0e825
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-09-05 15:08:15 -0700

    Add persistentChatHelper util

---
+++ chat-widget/src/components/livechatwidget/common/persistentChatHelper.ts
@@ -0,0 +1,19 @@
+import { isPersistentChatEnabled } from "./liveChatConfigUtils";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const shouldSetPreChatIfPersistentChat = async (chatSDK: any, showPreChat: boolean) => {
+    const persistentEnabled = await isPersistentChatEnabled(chatSDK);
+    let skipPreChat = false;
+    if (persistentEnabled) {
+        const reconnectableChatsParams = {
+            authenticatedUserToken: chatSDK.authenticatedUserToken as string
+        };
+
+        const reconnectableChatsResponse = await chatSDK.OCClient.getReconnectableChats(reconnectableChatsParams);
+        if (reconnectableChatsResponse && reconnectableChatsResponse.reconnectid) { // Skip rendering prechat on existing persistent chat session
+            skipPreChat = true;
+        }
+    }
+
+    return showPreChat && !skipPreChat;
+};
