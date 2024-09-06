d8a4cb460a2a34aa605f0b15f6e5ebc260534eea
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-09-06 11:24:08 -0700

    Add undefined check

--- chat-widget/src/components/livechatwidget/common/liveChatConfigUtils.ts
+++ chat-widget/src/components/livechatwidget/common/liveChatConfigUtils.ts
@@ -1,4 +1,5 @@
 import { ConversationMode } from "../../../common/Constants";
+import { isNullOrUndefined } from "../../../common/utils";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const isPostChatSurveyEnabled = async (chatSDK: any) : Promise<boolean> => {
@@ -9,5 +10,9 @@ export const isPostChatSurveyEnabled = async (chatSDK: any) : Promise<boolean> =
 };
 
 export const isPersistentChatEnabled = async (conversationMode: string | undefined): Promise<boolean> => {
+    if (isNullOrUndefined(conversationMode)) {
+        return false;
+    }
+
     return conversationMode?.toString().toLowerCase() === ConversationMode.Persistent;
 };
