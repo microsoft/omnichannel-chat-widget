ecd12a34c0b768891ca94a1435b360687cd81eb3
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-09-11 17:00:35 -0700

    Send echo activity

--- chat-widget/src/components/webchatcontainerstateful/common/DemoChatAdapter.ts
+++ chat-widget/src/components/webchatcontainerstateful/common/DemoChatAdapter.ts
@@ -4,14 +4,36 @@ import "rxjs/add/observable/of";
 import {  Message } from "botframework-directlinejs";
 import { Observable } from "rxjs/Observable";
 import MockAdapter from "./mockadapter";
+import { uuidv4 } from "@microsoft/omnichannel-chat-sdk";
 
 export class DemoChatAdapter extends MockAdapter {
     constructor() {
         super();
-        console.log("[DemoChatAdapter]");
+    }
+
+    // WebChat expects an "echo" activity to confirm the message has been sent successfully
+    private postEchoActivity(activity: Message): void {
+        const echoActivity: Message = {
+            ...activity,
+            id: uuidv4(),
+            from: {
+                ...activity.from,
+                id: "usedId",
+                name: "User",
+                role: "user"
+            }
+        };
+
+        setTimeout(() => {
+            this.activityObserver?.next(echoActivity); // mock message sent activity
+        }, 2000);
     }
 
     public postActivity(activity: Message): Observable<string> {
-        return super.postActivity(activity);
+        if (activity) {
+            this.postEchoActivity(activity);
+        }
+
+        return Observable.of(activity.id || "");
     }
 }
