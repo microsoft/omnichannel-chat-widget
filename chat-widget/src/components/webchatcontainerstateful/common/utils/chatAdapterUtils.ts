7758ea54f61aa439ac9d3f96bfc92bef28b149b7
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-09-13 13:47:15 -0700

    Add chatAdapterUtils

---
+++ chat-widget/src/components/webchatcontainerstateful/common/utils/chatAdapterUtils.ts
@@ -0,0 +1,31 @@
+import { uuidv4 } from "@microsoft/omnichannel-chat-sdk";
+import { Activity, Message, User } from "botframework-directlinejs";
+import { Subscriber } from "rxjs/Subscriber";
+
+export const customerUser: User = {
+    id: "usedId",
+    name: "User",
+    role: "user"
+};
+
+export const botUser: User = {
+    id: "botId",
+    name: "Bot",
+    role: "bot"
+};
+
+// WebChat expects an "echo" activity to confirm the message has been sent successfully
+export const postEchoActivity = (activityObserver: Subscriber<Activity> | undefined, activity: Message, user: User, delay = 1000): void => {
+    const echoActivity: Message = {
+        ...activity,
+        id: uuidv4(),
+        from: {
+            ...activity.from,
+            ...user
+        }
+    };
+
+    setTimeout(() => {
+        activityObserver?.next(echoActivity); // mock message sent activity
+    }, delay);
+};
