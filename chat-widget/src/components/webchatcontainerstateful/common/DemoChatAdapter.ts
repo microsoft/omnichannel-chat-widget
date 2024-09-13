7b887468f5c0c99f2e6fdb72d616ca26c21f8d8a
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-09-13 14:26:15 -0700

    Use attachmentActivityUtils

--- chat-widget/src/components/webchatcontainerstateful/common/DemoChatAdapter.ts
+++ chat-widget/src/components/webchatcontainerstateful/common/DemoChatAdapter.ts
@@ -4,6 +4,7 @@ import { Message } from "botframework-directlinejs";
 import { Observable } from "rxjs/Observable";
 import MockAdapter from "./mockadapter";
 import { customerUser, postBotMessageActivity, postBotAttachmentActivity, postBotTypingActivity, postEchoActivity, postSystemMessageActivity } from "./utils/chatAdapterUtils";
+import { createHeroCardAttachment, createJpgFileAttachment, createSigninCardAttachment, createThumbnailCardAttachment } from "./utils/attachmentActivityUtils";
 
 export class DemoChatAdapter extends MockAdapter {
     constructor() {
@@ -57,74 +58,19 @@ export class DemoChatAdapter extends MockAdapter {
                         postBotTypingActivity(this.activityObserver);
                         break;
                     case activity.text === "send attachment":
-                        postBotAttachmentActivity(this.activityObserver, [{
-                            contentType: "image/jpeg", 
-                            name: "600x400.jpg", 
-                            contentUrl: "https://raw.githubusercontent.com/microsoft/omnichannel-chat-sdk/e7e75d4ede351e1cf2e52f13860d2284848c4af0/playwright/public/images/600x400.jpg"}]);
+                        postBotAttachmentActivity(this.activityObserver, [createJpgFileAttachment()]);
                         break;
                     case activity.text === "send bot message":
                         postBotMessageActivity(this.activityObserver, "Hi, how can I help you?");
                         break;
                     case activity.text === "/card signin":
-                        postBotAttachmentActivity(this.activityObserver, [{
-                            contentType: "application/vnd.microsoft.card.signin",
-                            content: {
-                                text: "Please login",
-                                buttons: [
-                                    {
-                                        type: "signin",
-                                        title: "Signin",
-                                        value: "https://login.live.com/"
-                                    }
-                                ]
-                            }
-                        }]);
+                        postBotAttachmentActivity(this.activityObserver, [createSigninCardAttachment()]);
                         break;
                     case activity.text === "/card hero":
-                        postBotAttachmentActivity(this.activityObserver, [{
-                            contentType: "application/vnd.microsoft.card.hero",
-                            content: {
-                                buttons: [
-                                    {
-                                        title: "Bellevue",
-                                        type: "imBack",
-                                        value: "Bellevue"
-                                    },
-                                    {
-                                        title: "Redmond",
-                                        type: "imBack",
-                                        value: "Redmond"
-                                    },
-                                    {
-                                        title: "Seattle",
-                                        type: "imBack",
-                                        value: "Seattle"
-                                    }
-                                ],
-                                title: "Choose your location"
-                            }
-                        }]);
+                        postBotAttachmentActivity(this.activityObserver, [createHeroCardAttachment()]);
                         break;
                     case activity.text === "/card thumbnail":
-                        postBotAttachmentActivity(this.activityObserver, [{
-                            contentType: "application/vnd.microsoft.card.thumbnail",
-                            content: {
-                                title: "Microsoft",
-                                subtitle: "Our mission is to empower every person and every organization on the planet to achieve more.",
-                                text: "Microsoft creates platforms and tools powered by AI to deliver innovative solutions that meet the evolving needs of our customers. The technology company is committed to making AI available broadly and doing so responsibly, with a mission to empower every person and every organization on the planet to achieve more.",
-                                images: [{
-                                    alt: "Microsoft logo",
-                                    url: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31" // logo from https://microsoft.com
-                                }],
-                                buttons: [
-                                    {
-                                        title: "Learn more",
-                                        type: "openUrl",
-                                        value: "https://www.microsoft.com/"
-                                    }
-                                ]
-                            }
-                        }]);
+                        postBotAttachmentActivity(this.activityObserver, [createThumbnailCardAttachment()]);
                         break;
                     case activity.text.startsWith("/bot "):
                         postBotMessageActivity(this.activityObserver, activity.text.substring(5));
