c0e0a503702c96d25c662213214fb4591976a7e7
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-09-20 18:00:14 -0700

    Refactor

--- chat-widget/src/components/webchatcontainerstateful/common/DesignerChatAdapter.ts
+++ chat-widget/src/components/webchatcontainerstateful/common/DesignerChatAdapter.ts
@@ -10,22 +10,21 @@ export class DesignerChatAdapter extends MockAdapter {
         setTimeout(() => {
             postBotMessageActivity(this.activityObserver, "Id venenatis a condimentum vitae?", undefined, 0);
             this.postUserActivity("Diam donec adipiscing tristique risus nec feugiat in fermentum", 0);
-            postSystemMessageActivity(this.activityObserver, "We are finding the best agent for your inquiry, please hold ...", 0);
-            postSystemMessageActivity(this.activityObserver, "John has joined the chat", 0);
-            postBotMessageActivity(this.activityObserver, "Neque viverra justo nec ultrices dui sapien eget mi proin", undefined, 0);
+            postSystemMessageActivity(this.activityObserver, "We are finding the best agent for your inquiry, please hold ...", 100);
+            postSystemMessageActivity(this.activityObserver, "John has joined the chat", 100);
+            postBotMessageActivity(this.activityObserver, "Neque viverra justo nec ultrices dui sapien eget mi proin", undefined, 100);
         }, 1000);
     }
 
     private postUserActivity(text: string, delay = 1000) {
         setTimeout(() => {
-            this.postActivity({
+            postEchoActivity(this.activityObserver, {
+                text,
                 from: {
                     ...customerUser
                 },
-                text,
-                type: "message",
-                timestamp: new Date().toISOString()
-            });
+                type: "message"
+            }, customerUser, 0);
         }, delay);
     }
 
