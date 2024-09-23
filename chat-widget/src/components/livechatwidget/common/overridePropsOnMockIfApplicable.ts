9f679fc853da0bd0e25b3bafc9ec970f8765c278
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-09-23 10:19:32 -0700

    Disable webchat on 'designer' mock

--- chat-widget/src/components/livechatwidget/common/overridePropsOnMockIfApplicable.ts
+++ chat-widget/src/components/livechatwidget/common/overridePropsOnMockIfApplicable.ts
@@ -10,14 +10,30 @@ const overridePropsOnMockIfApplicable = (props: ILiveChatWidgetProps) => {
             props.webChatContainerProps.webChatProps = {};
         }
 
+        if (!props.webChatContainerProps.webChatStyles) {
+            props.webChatContainerProps.webChatStyles = {};
+        }
+
         if (!props.webChatContainerProps.webChatProps.overrideLocalizedStrings) {
             props.webChatContainerProps.webChatProps.overrideLocalizedStrings = {};
         }
 
-        props.webChatContainerProps.webChatProps.overrideLocalizedStrings = {
-            TEXT_INPUT_PLACEHOLDER: "Send a message . . .",
-            ...props.webChatContainerProps.webChatProps.overrideLocalizedStrings
-        }
+        props.webChatContainerProps = {
+            ...props.webChatContainerProps,
+            webChatProps: {
+                disabled: true,
+                ...props.webChatContainerProps.webChatProps,
+                overrideLocalizedStrings: {
+                    TEXT_INPUT_PLACEHOLDER: "Send a message . . .",
+                    ...props.webChatContainerProps.webChatProps.overrideLocalizedStrings
+                }
+            },
+            webChatStyles: {
+                hideUploadButton: false,
+                sendBoxBackground: "rgb(243, 242, 241)",
+                ...props.webChatContainerProps.webChatStyles
+            }
+        };
     }
 };
 
