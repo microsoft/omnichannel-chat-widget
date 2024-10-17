4f2c81ed9abc4381c4545f29c8f64e169aa44bb0
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-10-16 17:59:43 -0700

    Add ability to override default bot auth config

--- chat-widget/src/components/livechatwidget/common/createAdapter.ts
+++ chat-widget/src/components/livechatwidget/common/createAdapter.ts
@@ -6,9 +6,15 @@ import { ChatAdapterShim } from "./ChatAdapterShim";
 import { PauseActivitySubscriber } from "./ActivitySubscriber/PauseActivitySubscriber";
 import { BotAuthActivitySubscriber } from "./ActivitySubscriber/BotAuthActivitySubscriber";
 import { HiddenAdaptiveCardActivitySubscriber } from "./ActivitySubscriber/HiddenAdaptiveCardActivitySubscriber";
+import { ILiveChatWidgetProps } from "../interfaces/ILiveChatWidgetProps";
+
+const defaultBotAuthConfig = {
+    fetchBotAuthConfigRetries: 3,
+    fetchBotAuthConfigRetryInterval: 1000
+};
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const createAdapter = async (chatSDK: any) => {
+export const createAdapter = async (chatSDK: any, props?: ILiveChatWidgetProps) => {
     const chatAdapterOptionalParams = {
         IC3Adapter: {
             options: {
@@ -36,9 +42,14 @@ export const createAdapter = async (chatSDK: any) => {
     let adapter = await chatSDK.createChatAdapter(chatAdapterOptionalParams);
     //so far, there is no need to convert to the shim adapter when using visual tests
     if (chatSDK.isMockModeOn !== true) {
+        const botAuthActivitySubscriberOptionalParams = {
+            fetchBotAuthConfigRetries: props?.webChatContainerProps?.botAuthConfig?.fetchBotAuthConfigRetries || defaultBotAuthConfig.fetchBotAuthConfigRetries,
+            fetchBotAuthConfigRetryInterval: props?.webChatContainerProps?.botAuthConfig?.fetchBotAuthConfigRetryInterval || defaultBotAuthConfig.fetchBotAuthConfigRetryInterval
+        };
+
         adapter = new ChatAdapterShim(adapter);
         adapter.addSubscriber(new PauseActivitySubscriber());
-        adapter.addSubscriber(new BotAuthActivitySubscriber());
+        adapter.addSubscriber(new BotAuthActivitySubscriber(botAuthActivitySubscriberOptionalParams));
         // Remove this code after ICM ID:544623085 is fixed
         adapter.addSubscriber(new HiddenAdaptiveCardActivitySubscriber());
         return adapter.chatAdapter;
