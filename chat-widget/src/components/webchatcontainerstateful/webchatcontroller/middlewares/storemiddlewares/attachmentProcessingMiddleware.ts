f186a0c1eccc556e4a2e3315414f17dac5672d81
Edgar Lopez <elopezanaya@gmail.com>
2025-02-27 09:18:20 -0800

    attachment and messages work

--- chat-widget/src/components/webchatcontainerstateful/webchatcontroller/middlewares/storemiddlewares/attachmentProcessingMiddleware.ts
+++ chat-widget/src/components/webchatcontainerstateful/webchatcontroller/middlewares/storemiddlewares/attachmentProcessingMiddleware.ts
@@ -13,23 +13,27 @@ const createSendFileAction = (files: any[]) => {
         payload: {
             files
         },
-        type: WebChatActionType.WEB_CHAT_SEND_FILES
+        type: WebChatActionType.WEB_CHAT_SEND_MESSAGE
     };
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const attachmentProcessingMiddleware = ({ dispatch }: { dispatch: any }) => (next: any) => async (action: IWebChatAction) => {
-    if (action?.type === WebChatActionType.WEB_CHAT_SEND_FILES && action?.payload?.files?.length > 0) {
-        const files = action.payload.files;
+    console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware", action);
+    if (action?.type === WebChatActionType.WEB_CHAT_SEND_MESSAGE && action?.payload?.attachments?.length > 0) {
+        console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware - start");
+        const files = action.payload.attachments;
 
         if (files.length === 1) {
+            console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware - return");
             return next(action);
         }
 
         const dispatchAction = createSendFileAction(files.slice(0, files.length - 1));
         const nextAction = createSendFileAction([files[files.length - 1]]);
-
+        console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware - dispatchAction");
         await dispatch(dispatchAction);
+        console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware - nextAction");
         return next(nextAction);
     }
 
