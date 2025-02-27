ffc12c441c14a4951a0a8b23b7d238347d92cce0
Edgar Lopez <elopezanaya@gmail.com>
2025-02-27 09:21:16 -0800

    remove logs

--- chat-widget/src/components/webchatcontainerstateful/webchatcontroller/middlewares/storemiddlewares/attachmentProcessingMiddleware.ts
+++ chat-widget/src/components/webchatcontainerstateful/webchatcontroller/middlewares/storemiddlewares/attachmentProcessingMiddleware.ts
@@ -19,21 +19,16 @@ const createSendFileAction = (files: any[]) => {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const attachmentProcessingMiddleware = ({ dispatch }: { dispatch: any }) => (next: any) => async (action: IWebChatAction) => {
-    console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware", action);
     if (action?.type === WebChatActionType.WEB_CHAT_SEND_MESSAGE && action?.payload?.attachments?.length > 0) {
-        console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware - start");
         const files = action.payload.attachments;
 
         if (files.length === 1) {
-            console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware - return");
             return next(action);
         }
 
         const dispatchAction = createSendFileAction(files.slice(0, files.length - 1));
         const nextAction = createSendFileAction([files[files.length - 1]]);
-        console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware - dispatchAction");
         await dispatch(dispatchAction);
-        console.log("ELOPEZANAYA :: AttachmentProcessingMiddleware - nextAction");
         return next(nextAction);
     }
 
