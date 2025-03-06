49af3867a8648e8b5e0543d9c86140415cbc3909
Sonam Ligade <sonamligade@microsoft.com>
2025-03-06 14:20:29 +0530

    fixed download transcript issue for persistentChat on postChatSurvey

--- chat-widget/src/components/footerstateful/downloadtranscriptstateful/DownloadTranscriptStateful.tsx
+++ chat-widget/src/components/footerstateful/downloadtranscriptstateful/DownloadTranscriptStateful.tsx
@@ -12,6 +12,8 @@ import { NotificationScenarios } from "../../webchatcontainerstateful/webchatcon
 import { TelemetryHelper } from "../../../common/telemetry/TelemetryHelper";
 import createChatTranscript from "../../../plugins/createChatTranscript";
 import { executeReducer } from "../../../contexts/createReducer";
+import IChatToken from "@microsoft/omnichannel-chat-sdk/lib/external/IC3Adapter/IChatToken";
+import { uuidv4 } from "@microsoft/omnichannel-chat-sdk";
 
 const processDisplayName = (displayName: string): string => {
     // if displayname matches "teamsvisitor:<some alphanumeric string>", we replace it with "Customer"
@@ -177,6 +179,14 @@ export const downloadTranscript = async (facadeChatSDK: FacadeChatSDK, downloadT
     if (!liveChatContext) {
         const inMemoryState = executeReducer(state as ILiveChatWidgetContext, { type: LiveChatWidgetActionType.GET_IN_MEMORY_STATE, payload: null });
         liveChatContext = inMemoryState.domainStates.liveChatContext;
+        if (!liveChatContext) {
+            const chatToken = (state?.domainStates?.chatToken || inMemoryState.domainStates.chatToken) as IChatToken;
+            // create custom livechatcontext for persistent chat
+            liveChatContext = {
+                chatToken: chatToken,
+                requestId: chatToken.requestId || uuidv4(),
+            };
+        }
     }
 
     let data = await facadeChatSDK?.getLiveChatTranscript({liveChatContext});
