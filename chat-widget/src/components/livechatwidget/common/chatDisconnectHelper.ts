c7f360cef00459b8ede690042f2a825394451d5d
Edgar Lopez <elopezanaya@gmail.com>
2025-05-05 09:25:15 -0700

    adapt

--- chat-widget/src/components/livechatwidget/common/chatDisconnectHelper.ts
+++ chat-widget/src/components/livechatwidget/common/chatDisconnectHelper.ts
@@ -28,13 +28,10 @@ const handleChatDisconnect = (props: ILiveChatWidgetProps, state: ILiveChatWidge
         case false:
             // this means customr on purpose wants to hide the send box, so we should not override it
             if (props?.webChatContainerProps?.webChatStyles?.hideSendBox === true ) return;
+
             if (hideSendBoxOnConversationEnd !== false) {
                 // this should be enable only for those that dint disable the sandbox
-                if (props?.webChatContainerProps?.webChatStyles?.hideSendBox !== true){
                     setWebChatStyles((styles: StyleOptions) => { return { ...styles, hideSendBox: false }; });
-                } 
-
-                
             }
             break;
         default:
