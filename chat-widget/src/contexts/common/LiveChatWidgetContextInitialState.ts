75b479c5bef6e1fe334c1afb9a7bccd7425f7e73
Edgar Lopez <elopezanaya@gmail.com>
2025-05-05 15:50:44 -0700

    change for reload

--- chat-widget/src/contexts/common/LiveChatWidgetContextInitialState.ts
+++ chat-widget/src/contexts/common/LiveChatWidgetContextInitialState.ts
@@ -10,6 +10,22 @@ import { defaultMiddlewareLocalizedTexts } from "../../components/webchatcontain
 
 export const getLiveChatWidgetContextInitialState = (props: ILiveChatWidgetProps) => {
 
+    const outOfOfficeStateStrategy = (initialStateFromCache: ILiveChatWidgetContext, props: ILiveChatWidgetProps) => {
+        // Out of office hours may change from second to another, so we need to alway evaluate it from the props config
+        if (props.chatConfig?.LiveWSAndLiveChatEngJoin?.OutOfOperatingHours) {
+            const liveChatConfig = initialStateFromCache.domainStates?.liveChatConfig?.LiveWSAndLiveChatEngJoin;
+            if (liveChatConfig) {
+                liveChatConfig.OutOfOperatingHours =
+                    initialStateFromCache.appStates.outsideOperatingHours = liveChatConfig.OutOfOperatingHours;
+    
+                // this is needed to prevent the case of some errors preventing the ooo pane to show.
+                if (liveChatConfig.OutOfOperatingHours === true) {
+                    initialStateFromCache.appStates.conversationState = ConversationState.OutOfOffice;
+                }
+            }
+        }
+    };
+
     const widgetCacheId = getWidgetCacheIdfromProps(props);
     const cacheTtlInMins = props?.controlProps?.cacheTtlInMins ?? Constants.CacheTtlInMinutes;
     const storageType = props?.useSessionStorage === true ? StorageType.sessionStorage : StorageType.localStorage;
@@ -27,6 +43,13 @@ export const getLiveChatWidgetContextInitialState = (props: ILiveChatWidgetProps
         if (initialStateFromCache.appStates.conversationState === ConversationState.Prechat) {
             initialStateFromCache.appStates.conversationState = ConversationState.Closed;
         }
+
+        // we are always setting the chatConfig from the props to avoid any issues with the cache
+        initialStateFromCache.domainStates.liveChatConfig = props.chatConfig;
+
+        // Out of office hours may change from second to another, so we need to alway evaluate it from the props config
+        outOfOfficeStateStrategy(initialStateFromCache, props);
+
         return initialStateFromCache;
     }
 
@@ -52,12 +75,12 @@ export const getLiveChatWidgetContextInitialState = (props: ILiveChatWidgetProps
             startChatFailureType: StartChatFailureType.Generic
         },
         appStates: {
-            conversationState: ConversationState.Closed,
-            isMinimized: undefined,
+            conversationState: (props.chatConfig?.LiveWSAndLiveChatEngJoin?.OutOfOperatingHours.toString().toLowerCase() === "true")
+                ? ConversationState.OutOfOffice
+                : ConversationState.Closed,            isMinimized: undefined,
             previousElementIdOnFocusBeforeModalOpen: null,
             startChatFailed: false,
-            outsideOperatingHours: false,
-            preChatResponseEmail: "",
+            outsideOperatingHours: (props.chatConfig?.LiveWSAndLiveChatEngJoin?.OutOfOperatingHours.toString().toLowerCase() === "true"),            preChatResponseEmail: "",
             isAudioMuted: null,
             newMessage: false,
             hideStartChatButton: false,
