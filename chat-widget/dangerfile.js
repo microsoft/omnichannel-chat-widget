67cdf10f666768d97366485b76d089b6a490e338
Edgar Lopez <elopezanaya@gmail.com>
2025-05-16 15:50:38 -0700

    checking on something

--- chat-widget/dangerfile.js
+++ chat-widget/dangerfile.js
@@ -3,5 +3,8 @@ const hasChangelog = danger.git.modified_files.includes("CHANGE_LOG.md")
 
 // right now just checking for changelog, we could add more checks later
 if (!hasChangelog) {
+
+
+  
   error("Please add a changelog entry for your changes.");
 }
