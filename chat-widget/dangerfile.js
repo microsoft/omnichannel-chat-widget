0ca7ce70fceef196cee16c19d10da19af7bef11d
Edgar Lopez <elopezanaya@gmail.com>
2025-05-17 10:28:12 -0700

    enabled husky wwith lint

--- chat-widget/dangerfile.js
+++ chat-widget/dangerfile.js
@@ -1,9 +1,10 @@
 import { fail } from "danger";
 
 // check if CHANGE_LOG.md is modified, not added
-const hasChangelog = danger.git.modified_files.includes("CHANGE_LOG.md")
+// eslint-disable-next-line no-undef
+const hasChangelog = danger.git.modified_files.includes("CHANGE_LOG.md");
 
 // right now just checking for changelog, we could add more checks later
 if (!hasChangelog) {
-  fail("Please add a changelog entry for your changes.");
+    fail("Please add a changelog entry for your changes.");
 }
