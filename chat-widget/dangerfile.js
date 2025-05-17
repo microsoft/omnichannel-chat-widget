30cc5312e65c34823c63372b2cbc23d2d64efb9c
Edgar Lopez <elopezanaya@gmail.com>
2025-05-17 14:50:49 -0700

    cleanup

--- chat-widget/dangerfile.js
+++ chat-widget/dangerfile.js
@@ -5,12 +5,6 @@ import { fail } from "danger";
 const hasChangelog = danger.git.modified_files.includes("CHANGE_LOG.md");
 // eslint-disable-next-line no-undef
 const isEngineeringEnhancement = (danger.github.pr.title).includes("[eng]");
-
-
-
-// eslint-disable-next-line no-undef
-
-
 // right now just checking for changelog, we could add more checks later
 if (!hasChangelog && !isEngineeringEnhancement) {
     printErrorMessage("Please add a changelog entry for your changes.", "CHANGE_LOG_VALIDATION");
