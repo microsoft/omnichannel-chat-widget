ae2a9b71254be8384d3eaeb7d6064b8cfb3d6796
Edgar Lopez <elopezanaya@gmail.com>
2025-05-17 12:09:19 -0700

    cheking if the token has enough grants to get metadata

--- chat-widget/dangerfile.js
+++ chat-widget/dangerfile.js
@@ -4,10 +4,12 @@ import { fail } from "danger";
 // eslint-disable-next-line no-undef
 const hasChangelog = danger.git.modified_files.includes("CHANGE_LOG.md");
 // eslint-disable-next-line no-undef
-const isEngineeringEnhancement = (danger.github.pr.body + danger.github.pr.title).includes("[eng]");
+const isEngineeringEnhancement = (danger.github.pr.title).includes("[eng]");
 
 console.log("hasChangelog", hasChangelog);
 console.log("isEngineeringEnhancement", isEngineeringEnhancement);
+// eslint-disable-next-line no-undef
+console.log("modified_files", danger?.github?.pr?.title);
 
 // right now just checking for changelog, we could add more checks later
 if (!hasChangelog && !isEngineeringEnhancement) {
