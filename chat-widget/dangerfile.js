5c214913e2fae3e89bcb0e3a8b1fcaab3d861fcd
Edgar Lopez <elopezanaya@microsoft.com>
2025-05-22 13:28:51 -0700

    adding dangerjs (#649)
    Engineering enhancement, adding dangerjs and improve husky pre-commit

---
+++ chat-widget/dangerfile.js
@@ -0,0 +1,17 @@
+import { fail } from "danger";
+
+// check if CHANGE_LOG.md is modified, not added
+// eslint-disable-next-line no-undef
+const hasChangelog = danger.git.modified_files.includes("CHANGE_LOG.md");
+// eslint-disable-next-line no-undef
+const isEngineeringEnhancement = (danger.github.pr.title).includes("[eng]");
+// right now just checking for changelog, we could add more checks later
+if (!hasChangelog && !isEngineeringEnhancement) {
+    printErrorMessage("Please add a changelog entry for your changes.", "CHANGE_LOG_VALIDATION");
+}
+
+function printErrorMessage(message , rule) {
+    console.log(`********  ERROR : ${rule}  ***************`);
+    console.log(message);
+    fail(message);
+}
