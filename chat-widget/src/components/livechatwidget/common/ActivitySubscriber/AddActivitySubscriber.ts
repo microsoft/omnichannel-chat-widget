7b34a30bccd2764221607f8d6a34b8c63eba824f
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-07-10 16:30:57 -0700

    Add activity subscriber to add activities at runtime

---
+++ chat-widget/src/components/livechatwidget/common/ActivitySubscriber/AddActivitySubscriber.ts
@@ -0,0 +1,22 @@
+import ChatWidgetEvents from "../ChatWidgetEvents";
+import { IActivitySubscriber } from "./IActivitySubscriber";
+
+/**
+ * Subscriber that add new activities on-the-fly to WebChat. Activities are not persisted in the chat thread.
+ */
+export class AddActivitySubscriber implements IActivitySubscriber {
+    public observer: any;
+    
+    constructor() {
+        window.addEventListener(ChatWidgetEvents.ADD_ACTIVITY, (event: any) => {
+            console.log(event);
+            if (event?.detail?.payload?.activity) {                
+                this.observer.next(event.detail.payload.activity);
+            }
+        });
+    }
+
+    public async next(activity: any) {
+        return activity;
+    }
+}
