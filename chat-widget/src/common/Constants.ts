1fcdf546a4812470d8745ee11fa8c2a20cfa2185
Edgar Lopez <elopezanaya@gmail.com>
2025-07-15 05:35:34 -0700

    improve regex

--- chat-widget/src/common/Constants.ts
+++ chat-widget/src/common/Constants.ts
@@ -147,7 +147,7 @@ export class Constants {
 }
 
 export const Regex = class {
-    public static readonly EmailRegex = "^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$";
+    public static readonly EmailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 };
 
 export class HtmlIdNames {
