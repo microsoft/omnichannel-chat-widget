c59e41362e30c74cb95cf2fddde9b21d520e3241
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-07-18 11:10:04 -0700

    Refactor to use forEach

--- chat-components/src/services/EventQueue.ts
+++ chat-components/src/services/EventQueue.ts
@@ -14,19 +14,10 @@ class EventQueue {
 
     processEvents(deferTimeout = 0) {
         const dequeue = () => {
-            let queueSize = this.channelEventQueue.size; // Set queue size before processing to prevent infinite loop
-            while (queueSize > 0) {
-                const entries = this.channelEventQueue.entries();
-                const entry = entries.next(); // Process entry based on insertion order
-                if (entry?.value) {
-                    const [_, event] = entry.value;
-                    this.newMessage.next(event); // Post event directly instead of using pubChannel
-                    if (event.eventId) {
-                        this.channelEventQueue.delete(event?.eventId); // Remove event from queue regardless of outcome
-                    }
-                }
-                queueSize -= 1;
-            }
+            this.channelEventQueue.forEach((event, eventId) => { // Process entry based on insertion order
+                this.newMessage.next(event); // Post event directly instead of using pubChannel
+                this.channelEventQueue.delete(eventId); // Remove event from queue regardless of outcome
+            });
         }
 
         setTimeout(() => {
