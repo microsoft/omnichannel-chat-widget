242ecf58493a03fb982581665a1ecca2bc21c3ff
Sonam Ligade <sonamligade@microsoft.com>
2025-09-08 18:41:34 +0530

    removed chat-component changes

--- chat-components/src/components/suggestions/interfaces/ISuggestionsProps.ts
+++
@@ -1,9 +0,0 @@
-import { Slot } from "@fluentui/react-components";
-import { ISuggestionsStyleProps } from "./ISuggestionsStyleProps";
-import { ISuggestionsControlProps } from "./ISuggestionsControlProps";
-
-export interface ISuggestionsProps {
-    componentOverrides?: { action?: Slot<"span">; icon?: Slot<"span"> };
-    controlProps?: ISuggestionsControlProps;
-    styleProps?: ISuggestionsStyleProps;
-}
