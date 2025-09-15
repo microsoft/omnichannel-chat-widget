a7441154f26c764c455863472230b4daca0c7c48
Edgar Lopez <elopezanaya@gmail.com>
2025-09-15 14:47:56 -0700

    Refactor HistoryMessageTimestamp to improve timestamp rendering and logging, enhancing clarity for bot and user messages.

--- chat-widget/src/components/webchatcontainerstateful/webchatcontroller/middlewares/renderingmiddlewares/timestamps/HistoryMessageTimestamp.tsx
+++ chat-widget/src/components/webchatcontainerstateful/webchatcontroller/middlewares/renderingmiddlewares/timestamps/HistoryMessageTimestamp.tsx
@@ -3,36 +3,50 @@ import React, { Dispatch } from "react";
 import { ILiveChatWidgetAction } from "../../../../../../contexts/common/ILiveChatWidgetAction";
 import { ILiveChatWidgetContext } from "../../../../../../contexts/common/ILiveChatWidgetContext";
 import { Stack } from "@fluentui/react";
+import { defaultMiddlewareLocalizedTexts } from "../../../../common/defaultProps/defaultMiddlewareLocalizedTexts";
 import { defaultTimestampContentStyles } from "../defaultStyles/defaultTimestampContentStyles";
+import { getTimestampHourMinute } from "../../../../../../common/utils";
 import useChatContextStore from "../../../../../../hooks/useChatContextStore";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const HistoryMessageTimestamp = ({ args }: any) => {
-    const [state, ]: [ILiveChatWidgetContext, Dispatch<ILiveChatWidgetAction>] = useChatContextStore();
+    const [state,]: [ILiveChatWidgetContext, Dispatch<ILiveChatWidgetAction>] = useChatContextStore();
     const dir = state.domainStates.renderingMiddlewareProps?.timestampDir ?? state.domainStates.globalDir;
     const {
-        activity
+        activity: {
+            timestamp,
+            from: { name, role }
+        }
     } = args;
-    const { timestamp } = activity;
-    const { from } = activity;
-    const { name, role } = from;
 
-    console.log("LOPEZ :: Persistent history activity", activity);
-    console.log("LOPEZ :: Persistent history name", name);
     const contentStyles = {
         ...defaultTimestampContentStyles,
         ...state.domainStates.renderingMiddlewareProps?.timestampContentStyleProps
     };
 
-    const getLocalizedTimestamp = (timestamp: string) => {
-        const locale = navigator.language || "en-US";
-        const currentDate = new Date(timestamp);
-        return `${currentDate.toLocaleDateString(locale)} ${currentDate.toLocaleTimeString(locale)}`;
+    const getTimeElement = (timestamp: string): string | JSX.Element => {
+        const timeString = getTimestampHourMinute(timestamp);
+        const isAmPmFormat = timeString.toLowerCase().includes("am") || timeString.toLowerCase().includes("pm");
+
+        if (dir === "rtl" && isAmPmFormat) {
+            return <span dir="ltr">{getTimestampHourMinute(timestamp)}</span>;
+        } else {
+            return <span dir={dir}>{getTimestampHourMinute(timestamp)}</span>;
+        }
     };
 
+
+    console.log(
+        "LOPEZ :: HistoryMessageTimestamp", { name, role, timestamp }
+    );
     return (
-        <Stack style={contentStyles} dir={dir} horizontal>
-            {role === "bot" ? `${name} : ${getLocalizedTimestamp(timestamp)}` : `${getLocalizedTimestamp(timestamp)}`}
+        <Stack style={contentStyles} dir={dir}>
+            {role === "bot" && <>
+                <span dir={dir} aria-hidden="false">{name} : {getTimeElement(timestamp)}</span>
+            </>}
+            {role === "user" && <>
+                <span aria-hidden="false" dir={dir}> {getTimeElement(timestamp)}{" - "}
+                    {state.domainStates.middlewareLocalizedTexts?.MIDDLEWARE_MESSAGE_DELIVERED ?? defaultMiddlewareLocalizedTexts.MIDDLEWARE_MESSAGE_DELIVERED}</span>            </>}
         </Stack>
     );
 };
