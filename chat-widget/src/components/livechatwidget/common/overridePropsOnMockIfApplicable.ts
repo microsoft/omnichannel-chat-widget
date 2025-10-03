77744360ef93b7538df68cd9c8870171921b8b68
Bella Xie <xxhice@gmail.com>
2025-10-03 08:08:43 -0700

    apply pr comments

--- chat-widget/src/components/livechatwidget/common/overridePropsOnMockIfApplicable.ts
+++ chat-widget/src/components/livechatwidget/common/overridePropsOnMockIfApplicable.ts
@@ -22,7 +22,7 @@ const overridePropsOnMockIfApplicable = (props: ILiveChatWidgetProps) => {
         props.webChatContainerProps = {
             ...props.webChatContainerProps,
             webChatProps: {
-                disabled: props?.mock?.mockMessages && props?.mock?.mockMessages.length > 0 ? false : true,
+                disabled: !(props?.mock?.mockMessages?.length),
                 ...props.webChatContainerProps.webChatProps,
                 overrideLocalizedStrings: {
                     TEXT_INPUT_PLACEHOLDER: "Send a message . . .",
@@ -37,5 +37,6 @@ const overridePropsOnMockIfApplicable = (props: ILiveChatWidgetProps) => {
         };
     }
 };
+
 
 export default overridePropsOnMockIfApplicable;
