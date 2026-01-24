87e1e1a2bbd6f52ea385c9426b0875680343fe25
Sunny Nayak <sunaya@microsoft.com>
2026-01-24 12:25:33 -0800

    Fix storybook build related issues

--- chat-widget/.storybook/main.cjs
+++ chat-widget/.storybook/main.cjs
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.tsx"
@@ -6,22 +8,37 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     '@storybook/addon-a11y',
-    '@storybook/addon-knobs',
-    'storybook-addon-playwright/preset',
-    'storybook-addon-playwright/register'
+    "@storybook/addon-webpack5-compiler-babel",
+    "@chromatic-com/storybook"
   ],
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  },
+  // Disable telemetry to prevent RangeError: Invalid string length in CI
+  "core": {
+    "disableTelemetry": true
+  },
   "webpackFinal": async (config, { configType }) => {
     config.module.rules.push({
       test: /\.(js|jsx)$/,
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
+          presets: ['@babel/preset-env'],
+          compact: false // Suppress "deoptimised" warnings for large files
         }
       }
     });
 
-    // config.resolve.modules = ["node_modules", path.resolve(__dirname, "../src")];
+    // Fix swiper ESM exports issue - redirect all swiper imports to the bundle
+    const swiperPath = path.resolve(__dirname, '../node_modules/swiper');
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'swiper/modules': path.join(swiperPath, 'swiper-bundle.esm.js'),
+      'swiper/css': path.join(swiperPath, 'swiper.min.css'),
+      'swiper': path.join(swiperPath, 'swiper-bundle.esm.js'),
+    };
 
     // Return the altered config
     return config;
