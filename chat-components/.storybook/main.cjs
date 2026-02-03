0e7ab45323368e26423a59a97d586ae6f9c3caaf
Bradley Hanson <brhanso@microsoft.com>
2026-02-03 12:53:29 -0800

    try add this back

--- chat-components/.storybook/main.cjs
+++ chat-components/.storybook/main.cjs
@@ -6,7 +6,9 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     '@storybook/addon-a11y',
-    '@storybook/addon-knobs'
+    '@storybook/addon-knobs',
+    'storybook-addon-playwright/preset',
+    'storybook-addon-playwright/register'
   ],
   "framework": {
     "name": "@storybook/react-webpack5",
