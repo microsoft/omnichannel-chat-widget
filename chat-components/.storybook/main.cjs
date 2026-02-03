f36a534597b59e321dcedffb6e122fa85858df19
Bradley Hanson <brhanso@microsoft.com>
2026-02-03 14:34:54 -0800

    fix storybook

--- chat-components/.storybook/main.cjs
+++ chat-components/.storybook/main.cjs
@@ -7,11 +7,18 @@ module.exports = {
     "@storybook/addon-essentials",
     '@storybook/addon-a11y',
     '@storybook/addon-knobs',
-    'storybook-addon-playwright/preset',
-    'storybook-addon-playwright/register'
+    'storybook-addon-playwright/preset'
   ],
   "framework": {
     "name": "@storybook/react-webpack5",
     "options": {}
+  },
+  webpackFinal: async (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+    };
+    return config;
   }
 }
