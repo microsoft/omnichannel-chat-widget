c06223e89f4a6f84e1655841b2ce9da9945685d6
Bradley Hanson <brhanso@microsoft.com>
2026-02-04 17:01:55 -0800

    Fix unit

--- chat-widget/jest.config.unit.cjs
+++ chat-widget/jest.config.unit.cjs
@@ -6,15 +6,14 @@ module.exports = {
         "(/__tests__/.*|(\\.|/)(visual.test|visual.spec))\\.[jt]sx?$",
         "automation_tests"
     ],
-    "transformIgnorePatterns": [
-        "node_modules/(?!(lodash-es|swiper)/)"
-    ],
+    "transformIgnorePatterns": [],
     "transform": {
         "^.+\\.(js|jsx|ts|tsx|mjs)$": "babel-jest",
         ".+\\.(css|scss|png|jpg|svg|gif)$": "jest-transform-stub"
     },
     "moduleNameMapper": {
-        "^swiper/modules$": "swiper"
+        "^swiper/modules$": "swiper",
+        "^swiper$": "<rootDir>/node_modules/swiper/swiper.esm.js"
     },
     setupFiles: ["./jest.setup.js"]
 
